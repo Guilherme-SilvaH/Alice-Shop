736c6a282c6df64c59e72288a132b2a7ddc4063d
Guilherme-SilvaH <guihenriquesilva10@gmail.com>
2024-07-02 17:06:41 -0300

    Send

---
+++ src/components/Send/Send.tsx
@@ -0,0 +1,19 @@
+import "./Send.scss";
+
+const Send = () => {
+  return (
+    <div className="container-send">
+      <div className="row justify-content-between">
+        <div className="col-4">
+          Enviamos para todo Brasil
+          <p>Via Correios.</p>
+        </div>
+        <div className="col-4">
+          Segurança Garantida
+          <p>Site 100% seguro</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Send;
