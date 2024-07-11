606cd7f555e5cd22b08637638a42cefc99d35208
Guilherme-SilvaH <guihenriquesilva10@gmail.com>
2024-07-11 20:50:52 -0300

    a

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import App from "./app/App.tsx";
+import App from "./app/App";
 
 ReactDOM.render(
   <BrowserRouter>
