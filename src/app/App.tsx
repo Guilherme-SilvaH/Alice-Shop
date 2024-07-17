5025d5796df13c4c1b907803a7ff478a2a9887d3
Guilherme-SilvaH <guihenriquesilva10@gmail.com>
2024-07-17 15:20:56 -0300

    commponets

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,9 @@
-import CardsContainer from "@/Components/CardsContainer/CardsContainer";
-import Header from "@/Components/Header/Header";
-import CardDetails from "@/Components/Pages/CardDetails";
-import Home from "@/Components/Pages/Home";
-import { Cards } from "@/Data/CardsData";
-import { CardsProvider } from "@/Hooks/CardsProvider";
+import CardsContainer from "../Components/CardsContainer/CardsContainer";
+import Header from "../Components/Header/Header";
+import CardDetails from "../Components/Pages/CardDetails";
+import Home from "../Components/Pages/Home";
+import { Cards } from "../Data/CardsData";
+import { CardsProvider } from "../Hooks/CardsProvider";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
